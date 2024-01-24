a191bdfd29e4c001bd53f7adbcaaa7d6747b58be
minsu <zmfjtnl1@naver.com>
2024-01-25 00:34:36 +0900

    upload AccountAdaptorProperties

--- src/main/java/com/nhnacademy/minidooray/gateway/config/AccountAdaptorProperties.java
+++ src/main/java/com/nhnacademy/minidooray/gateway/config/AccountAdaptorProperties.java
@@ -1,11 +1,12 @@
 package com.nhnacademy.minidooray.gateway.config;
 
+import javax.validation.constraints.NotNull;
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 @Data
-@ConfigurationProperties(prefix = "com.nhnacademy.minidooray.gateway")
+@ConfigurationProperties(prefix = "com.nhnacademy.minidooray.gateway.account")
 public class AccountAdaptorProperties {
-
+    @NotNull
     String address;
 }
