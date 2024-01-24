252b9463fe3fff6eb1725363257468f7964733c9
minsu <zmfjtnl1@naver.com>
2024-01-25 00:38:18 +0900

    upload TaskAdaptorProperties

--- src/main/java/com/nhnacademy/minidooray/gateway/config/TaskAdaptorProperties.java
+++ src/main/java/com/nhnacademy/minidooray/gateway/config/TaskAdaptorProperties.java
@@ -1,4 +1,12 @@
 package com.nhnacademy.minidooray.gateway.config;
 
+import javax.validation.constraints.NotNull;
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@Data
+@ConfigurationProperties(prefix = "com.nhnacademy.minidooray.gateway.task")
 public class TaskAdaptorProperties {
+    @NotNull
+    private String address;
 }
