ac9e0c8da807c7132a34f65f169950c75e432d35
minsu <zmfjtnl1@naver.com>
2024-01-25 14:39:37 +0900

    create LoginController and login-form.html

--- src/main/java/com/nhnacademy/minidooray/gateway/controller/LoginController.java
+++ src/main/java/com/nhnacademy/minidooray/gateway/controller/LoginController.java
@@ -1,4 +1,21 @@
 package com.nhnacademy.minidooray.gateway.controller;
 
+import com.nhnacademy.minidooray.gateway.service.AccountService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/login")
 public class LoginController {
+    private final AccountService accountService;
+
+    public LoginController(AccountService accountService) {
+        this.accountService = accountService;
+    }
+
+    @GetMapping
+    public String viewLoginForm() {
+        return "login-form";
+    }
 }
