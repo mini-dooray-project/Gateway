82b0d181515e016814a251099e986edf8dcfbfbc
minsu <zmfjtnl1@naver.com>
2024-01-25 14:50:05 +0900

    commit

---
+++ src/main/java/com/nhnacademy/minidooray/gateway/interceptor/LoginCheckInterceptor.java
@@ -0,0 +1,18 @@
+package com.nhnacademy.minidooray.gateway.interceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+public class LoginCheckInterceptor implements HandlerInterceptor {
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        HttpSession session = request.getSession();
+        if (session.getAttribute("login") == null) {
+            response.sendRedirect("/login");
+            return false;
+        }
+        return true;
+    }
+}
