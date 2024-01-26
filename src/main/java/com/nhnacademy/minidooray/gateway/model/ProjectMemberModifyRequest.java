29de0eeaa784771b9baef2c4fa4047d359afe7fd
minsu <zmfjtnl1@naver.com>
2024-01-27 01:24:16 +0900

    upload ProjectMemberModifyRequest

--- src/main/java/com/nhnacademy/minidooray/gateway/model/ProjectMemberModifyRequest.java
+++ src/main/java/com/nhnacademy/minidooray/gateway/model/ProjectMemberModifyRequest.java
@@ -1,4 +1,10 @@
 package com.nhnacademy.minidooray.gateway.model;
 
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+@Data
+@RequiredArgsConstructor
 public class ProjectMemberModifyRequest {
+    private Long authorityId;
 }
