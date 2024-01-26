acc0889282a7009baec9ae719b0a4611819bf8db
minsu <zmfjtnl1@naver.com>
2024-01-27 01:47:04 +0900

    Upload CommentResponse

--- src/main/java/com/nhnacademy/minidooray/gateway/model/CommentResponse.java
+++ src/main/java/com/nhnacademy/minidooray/gateway/model/CommentResponse.java
@@ -1,2 +1,15 @@
-package com.nhnacademy.minidooray.gateway.model;public class CommentResponse {
+package com.nhnacademy.minidooray.gateway.model;
+
+import java.time.LocalDateTime;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+@Data
+@RequiredArgsConstructor
+public class CommentResponse {
+    private Long commentId;
+    private Long taskId;
+    private String registrantAccount;
+    private LocalDateTime createdDate;
+    private String content;
 }
