ef62a89b2c55d4fc5c1509fc6888d79977744fef
minsu <zmfjtnl1@naver.com>
2024-01-27 01:38:20 +0900

    upload TaskRequest

--- src/main/java/com/nhnacademy/minidooray/gateway/model/TaskRequest.java
+++ src/main/java/com/nhnacademy/minidooray/gateway/model/TaskRequest.java
@@ -1,2 +1,17 @@
-package com.nhnacademy.minidooray.gateway.model;public class TaskRequest {
+package com.nhnacademy.minidooray.gateway.model;
+
+import java.time.LocalDateTime;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+@Data
+@RequiredArgsConstructor
+public class TaskRequest {
+    Long milestoneId;
+    Long projectId;
+    String title;
+    String content;
+    String registrantAccount;
+    LocalDateTime expireDate;
+
 }
