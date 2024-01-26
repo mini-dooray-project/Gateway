a9939ba88ae9567586554db25029bb7e52096e1e
minsu <zmfjtnl1@naver.com>
2024-01-27 01:17:30 +0900

    upload ProjectMemberRegisterRequest

--- src/main/java/com/nhnacademy/minidooray/gateway/model/ProjectMemberRegisterRequest.java
+++ src/main/java/com/nhnacademy/minidooray/gateway/model/ProjectMemberRegisterRequest.java
@@ -1,2 +1,12 @@
-package com.nhnacademy.minidooray.gateway.model;public class ProjectMemberRegisterRequest {
+package com.nhnacademy.minidooray.gateway.model;
+
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+@Data
+@RequiredArgsConstructor
+public class ProjectMemberRegisterRequest {
+    private String memberId;
+    private Long projectId;
+    private Long authorityId;
 }
