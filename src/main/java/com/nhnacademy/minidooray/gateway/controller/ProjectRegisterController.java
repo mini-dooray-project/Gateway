2ea72fc4432b33bf9c69aea0de3128c25ca5b3b0
minsu <zmfjtnl1@naver.com>
2024-01-26 21:40:45 +0900

    create Controller and message bundle and html

---
+++ src/main/java/com/nhnacademy/minidooray/gateway/controller/ProjectRegisterController.java
@@ -0,0 +1,4 @@
+package com.nhnacademy.minidooray.gateway.controller;
+
+public class ProjectRegisterController {
+}
--- src/main/java/com/nhnacademy/minidooray/gateway/model/ProjectRegisterRequest.java
+++ src/main/java/com/nhnacademy/minidooray/gateway/model/ProjectRegisterRequest.java
@@ -1,5 +1,6 @@
 package com.nhnacademy.minidooray.gateway.model;
 
 public class ProjectRegisterRequest {
+    String projectName;
 
 }
