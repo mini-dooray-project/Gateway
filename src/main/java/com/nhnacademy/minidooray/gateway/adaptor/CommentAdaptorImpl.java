9c5bf3550114d5817477e7adbdefb427bd5883a5
minsu <zmfjtnl1@naver.com>
2024-01-27 01:56:25 +0900

    add method

--- src/main/java/com/nhnacademy/minidooray/gateway/adaptor/CommentAdaptorImpl.java
+++ src/main/java/com/nhnacademy/minidooray/gateway/adaptor/CommentAdaptorImpl.java
@@ -1,2 +1,58 @@
-package com.nhnacademy.minidooray.gateway.adaptor;public class CommentAdaptorImpl {
+package com.nhnacademy.minidooray.gateway.adaptor;
+
+import com.nhnacademy.minidooray.gateway.config.TaskAdaptorProperties;
+import com.nhnacademy.minidooray.gateway.model.CommentRequest;
+import com.nhnacademy.minidooray.gateway.model.CommentResponse;
+import com.nhnacademy.minidooray.gateway.model.ProjectRequest;
+import com.nhnacademy.minidooray.gateway.model.ProjectResponse;
+import java.util.List;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.*;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+@Component
+public class CommentAdaptorImpl implements CommentAdaptor {
+    private RestTemplate restTemplate;
+    private TaskAdaptorProperties taskAdaptorProperties;
+
+    @Override
+    public List<CommentResponse> getCommentByTask(Long taskId) {
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.setAccept(List.of(MediaType.APPLICATION_JSON));
+
+        HttpEntity<String> requestEntity = new HttpEntity<>(headers);
+
+        //responseEntity
+        ResponseEntity<List<CommentResponse>> exchange = restTemplate.exchange(taskAdaptorProperties.getAddress() + "/api/comments/{taskId}",
+                HttpMethod.GET,
+                requestEntity,
+                new ParameterizedTypeReference<>() {
+                }, taskId);
+        if (exchange.getStatusCode() != HttpStatus.OK) {
+            throw new RuntimeException();
+        }
+        return exchange.getBody();
+    }
+
+    @Override
+    public CommentResponse createComment(CommentRequest commentRequest) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.setAccept(List.of(MediaType.APPLICATION_JSON));
+
+        HttpEntity<ProjectRequest> requestEntity = new HttpEntity<>(request, headers);
+
+        ResponseEntity<ProjectResponse> exchange = restTemplate.exchange(taskAdaptorProperties.getAddress() + "/api/projects",
+                HttpMethod.POST,
+                requestEntity,
+                new ParameterizedTypeReference<>() {
+                });
+        if (exchange.getStatusCode() != HttpStatus.OK) {
+            throw new RuntimeException();
+        }
+        return exchange.getBody();
+    }
 }
