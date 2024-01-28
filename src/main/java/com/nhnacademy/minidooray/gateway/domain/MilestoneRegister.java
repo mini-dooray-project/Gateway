9dc13ca4979078f53f0f0bce90bd54ed9bb81e86
th915 <97163658+th915@users.noreply.github.com>
2024-01-28 22:09:20 +0900

    upload html file

--- src/main/java/com/nhnacademy/minidooray/gateway/domain/MilestoneRegister.java
+++ src/main/java/com/nhnacademy/minidooray/gateway/domain/MilestoneRegister.java
@@ -4,12 +4,15 @@ import java.time.LocalDateTime;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class MilestoneRegister {
     private String milestoneName;
+    @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm")
     private LocalDateTime startDate;
+    @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm")
     private LocalDateTime milestoneExpireDate;
 }
---
+++ src/main/java/com/nhnacademy/minidooray/gateway/model/RegisterProjectMember.java
@@ -0,0 +1,12 @@
+package com.nhnacademy.minidooray.gateway.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class RegisterProjectMember {
+    private String accountId;
+}
