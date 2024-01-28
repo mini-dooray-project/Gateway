6b61ca70938dc894fe7857bb4e8d42029e670dc5
minm063 <jangmin063@gmail.com>
2024-01-28 23:47:26 +0900

    feat: swagger 추가

--- src/main/java/com/nhnacademy/minidooray/gateway/config/WebMvcConfig.java
+++ src/main/java/com/nhnacademy/minidooray/gateway/config/WebMvcConfig.java
@@ -1,19 +1,25 @@
 package com.nhnacademy.minidooray.gateway.config;
 
 import com.nhnacademy.minidooray.gateway.interceptor.LoginCheckInterceptor;
+import com.nhnacademy.minidooray.gateway.interceptor.ProjectAuthorityCheckInterceptor;
+import com.nhnacademy.minidooray.gateway.service.ProjectMemberService;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 
 @Configuration // 제가 세팅한 것만
 public class WebMvcConfig implements WebMvcConfigurer {
+    private final ProjectMemberService projectMemberService;
+
+    public WebMvcConfig(ProjectMemberService projectMemberService) {
+        this.projectMemberService = projectMemberService;
+    }
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         LoginCheckInterceptor loginCheckInterceptor = new LoginCheckInterceptor();
         registry.addInterceptor(loginCheckInterceptor).addPathPatterns("/client/projects/**");
-        registry.addInterceptor(new LocaleChangeInterceptor())
+        registry.addInterceptor(new ProjectAuthorityCheckInterceptor(projectMemberService))
                 .addPathPatterns("/client/projects/**")
                 .excludePathPatterns("/client/projects");
     }
