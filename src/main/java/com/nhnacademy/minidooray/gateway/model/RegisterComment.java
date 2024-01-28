062114a3b36836b938f6fa855608550697910fe4
minsu <zmfjtnl1@naver.com>
2024-01-28 15:48:46 +0900

    upload REgister Comment

--- src/main/java/com/nhnacademy/minidooray/gateway/model/RegisterComment.java
+++ src/main/java/com/nhnacademy/minidooray/gateway/model/RegisterComment.java
@@ -1,2 +1,13 @@
-package com.nhnacademy.minidooray.gateway.model;public class RegisterComment {
+package com.nhnacademy.minidooray.gateway.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class RegisterComment {
+    private String content;
+
 }
--- src/main/java/com/nhnacademy/minidooray/gateway/service/CommentServiceImpl.java
+++ src/main/java/com/nhnacademy/minidooray/gateway/service/CommentServiceImpl.java
@@ -1,2 +1,36 @@
-package com.nhnacademy.minidooray.gateway.service;public class CommentServiceImpl {
+package com.nhnacademy.minidooray.gateway.service;
+
+import com.nhnacademy.minidooray.gateway.adaptor.CommentAdaptor;
+import com.nhnacademy.minidooray.gateway.model.CommentResponse;
+import java.util.List;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CommentServiceImpl implements CommentService {
+    private CommentAdaptor commentAdaptor;
+
+    public CommentServiceImpl(CommentAdaptor commentAdaptor) {
+        this.commentAdaptor = commentAdaptor;
+    }
+
+    @Override
+    public List<CommentResponse> getCommentByTask(Long taskId) {
+        return commentAdaptor.getCommentByTask(taskId);
+    }
+
+    @Override
+    public CommentResponse createComment() {
+
+        return null;
+    }
+
+    @Override
+    public CommentResponse updateComment() {
+        return null;
+    }
+
+    @Override
+    public void deleteComment() {
+
+    }
 }
